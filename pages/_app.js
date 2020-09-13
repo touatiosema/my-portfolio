c8b7d5fb083e4dcd1ece9d920835d4812deac833
touati osema <go_touati@esi.dz>
2020-09-13 08:58:22 +0200

    adding home page

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import Head from "next/head";
+import NavBar from "./components/NavBar";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Osema TOUATI - Student of software engineering</title>
+        <link rel="icon" href="/logo.png" />
+      </Head>
+      <NavBar />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,49 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
-
+import styles from "../styles/Home.module.scss";
+import Link from "next/link";
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
+    <div className={styles.content}>
+      <h2>Hi, I'm Osema.</h2>
+      <h1>
+        Student of Software Engineering at{" "}
         <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+          className="selected"
+          href="https://enseirb-matmeca.bordeaux-inp.fr/fr"
+          target="_black"
         >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+          ENSEIRB-MATMECA{" "}
         </a>
-      </footer>
+        , and{" "}
+        <a href="http://www.esi.dz" target="_blank" className="react">
+          ESI
+        </a>{" "}
+        before that.
+      </h1>
+      <div>
+        <h3>
+          <span className="react">React </span>
+          enthousiaste,
+          <span className="next">Nextjs</span> lover.
+        </h3>
+        <h3>
+          Passionate about efficient app development, architectural solutions
+          and design patterns.
+        </h3>
+        <h3>
+          I always enjoy a challenge.
+          <div className={styles.quotes}>
+            Accept the challenges so that you can feel the exhilaration of
+            victory
+          </div>
+        </h3>
+        <h3 className="mt-5">
+          <Link href="/about">
+            <a className="selected" href="/about">
+              Learn more about me
+            </a>
+          </Link>
+        </h3>
+      </div>
     </div>
-  )
+  );
 }
